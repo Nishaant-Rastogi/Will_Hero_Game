3279337eeea2e1aae2a57e14620283e3e20f58b2
Shivaansh Mital <shivaansh20122@iiitd.ac.in>
2021-12-03 00:16:53 +0530

    a little

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,43 @@
-package sample;
-import java.awt.event.MouseEvent;
-import java.io.IOException;
 
+package sample;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class Controller {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private AnchorPane gameRoot;
 
     public void pauseMenu(javafx.scene.input.MouseEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("PauseMenu.fxml"));
@@ -38,5 +61,13 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }
+    public void game(javafx.scene.input.MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 }
--- src/sample/Island.java
+++ src/sample/Island.java
@@ -3,6 +3,7 @@ package sample;
 public class Island extends Game_objects implements Jumpable{
     private int length;
     public Island(int l){
+        super(50,25,0,0,"../assets/island_1.png",100,100);
         this.length=l;
     }
 
