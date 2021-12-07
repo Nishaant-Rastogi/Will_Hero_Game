81a36c344dd6cb05c06eb66266a12055eed817f1
Nishaant-Rastogi <nishaant220091@iiitd.ac.in>
2021-12-07 09:43:48 +0530

    Pause Button Continuity Added

--- src/sample/PauseMenuController.java
+++ src/sample/PauseMenuController.java
@@ -22,6 +22,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PauseMenuController {
     @FXML
@@ -33,7 +34,9 @@ public class PauseMenuController {
     private AnchorPane pauseMenu;
     private MediaPlayer mediaPlayer;
     private MainMenuController mainMenuController;
+    private GamePlayController gamePlayController;
     public void initData(GamePlayController gamePlayController, AnchorPane menu){
+        this.gamePlayController = gamePlayController;
         this.root = gamePlayController.getRoot();
         this.time = gamePlayController.getTime();
         this.pauseMenu = menu;
@@ -61,7 +64,7 @@ public class PauseMenuController {
             }
         }
     }
-    public void musicButton(javafx.scene.input.MouseEvent event) throws IOException{
+    public void musicButton(MouseEvent event) throws IOException{
         if(mediaPlayer.isMute()) {
             Image music = new Image(new File("src/assets/MusicButton.png").toURI().toString());
             this.music.setImage(music);
@@ -71,6 +74,32 @@ public class PauseMenuController {
             this.music.setImage(musicOn);
             mediaPlayer.setMute(true);
         }
+    }
+    public void reloadButton(MouseEvent mouseEvent) throws IOException{
+        Group root = new Group();
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("GamePlay.fxml"));
+        stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+
+        //GameObjects Added
+        String islandPath = "src/assets/island_1.png";
+        String smallIslandPath = "src/assets/island_4.png";
+        Island island = new Island(50, 180, 0, 0.5, islandPath, 320, 350, -1);
+        Island bigIsland = new Island(400,100,0,0.5,smallIslandPath,400,520,-1);
+        ArrayList<Island> islands = new ArrayList<>();
+        ArrayList<Orc> orcs = new ArrayList<>();
+        islands.add(island);islands.add(bigIsland);
+        Hero hero = new Hero(100,250,0,2,70,70);
+        Orc greenOrc = new Normal_G_Orc(450,250,0,2.5,70,70);
+        orcs.add(greenOrc);
+        Obstacle tnt = new TNT(280,250,0,1,70,70);
+        Weapon_chest chest = new Weapon_chest(580,100,0,0,100,70);
+
+        root.getChildren().add(fxmlLoader.load());
+        GamePlayController gamePlayController = fxmlLoader.getController();
+        gamePlayController.initData(root, hero, islands, tnt, orcs, chest, mediaPlayer);
+        scene = new Scene(root,800,600);
+        stage.setScene(scene);
+        stage.show();
     }
     public void home(MouseEvent mouseEvent) throws IOException {
         this.mediaPlayer.dispose();
